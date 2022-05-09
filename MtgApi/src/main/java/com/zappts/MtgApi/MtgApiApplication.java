6e6515a38eebd26e28056a82025d82fbe6fd93c6
Alarico Song <alaricosong@gmail.com>
2022-05-09 12:31:08 -0300

    First commit

---
+++ MtgApi/src/main/java/com/zappts/MtgApi/MtgApiApplication.java
@@ -0,0 +1,13 @@
+package com.zappts.MtgApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgApiApplication.class, args);
+	}
+
+}
